27a053a2939898e5d723d0909701783bceb0d2a3
Saniddhya Nigam <saniddhya_nigam@epam.com>
2022-07-14 14:54:47 +0530

    gh pages

--- src/Contants/WorkData.js
+++ src/Contants/WorkData.js
@@ -2,6 +2,9 @@ import Image1 from "../Assets&Images/InfiniteScroll.gif"
 import Image2 from "../Assets&Images/YoutubeSearch.gif"
 import Image3 from "../Assets&Images/QuoteGenerator.gif"
 import Nodeimage from "../Assets&Images/Node.png"
+import Ecom from "../Assets&Images/EcomHome.gif"
+import Todo from "../Assets&Images/ToDolist.png"
+import Usermng from "../Assets&Images/UserManag.png"
 
 export const Technologies =[
     {"Name":"JavaScript"},
@@ -26,37 +29,32 @@ export const Projects ={
         "Name": "Quote Generator",
         "Disc":"Hi",
         "Image":Image3
-    },
-    {
-        "Name": "Quote Generator",
-        "Disc":"Hi",
-        "Image":Image3
     }
 ],
 
     "ReactJs":[
         {
             "Name":"Ecommerce Website",
-            "Disc":"Hi",
-            "Image":Image1
+            "Disc":"Ecommerce-Website-with-React-Redux with JSON server to Fetch Mock Data",
+            "Image":Ecom
         },
         {
-            "Name":"Ecommerce Website",
+            "Name":"TODO List",
             "Disc":"Hi",
-            "Image":Image1
+            "Image":Todo
         }
     ],
     "Angular":[
         {
             "Name":"User Management Webpage",
             "Disc":"Hi",
-            "Image":"Hi"
+            "Image":Usermng
         }
     ],
     "NodeJs":[
         {
             "Name":"CRUD-REST-API-with-NODE-JS-Express-and-POSTGRES-DB",
-            "Disc":"Hi",
+            "Disc":"CRUD REST SERVICE WITH VALIDATION and POSTGRESQL LAYERED ARCHITECTURE",
             "Image":Nodeimage
         }
     ]
